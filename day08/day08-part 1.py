3888e0a6738503ac131432fd7b0a32e7c55c3011
JarroVGIT <jarro@vanginkel-it.nl>
2024-12-19 13:30:59 +0100

    Day 8

--- day08/day08-part 1.py
+++ day08/day08-part 1.py
@@ -6,5 +6,34 @@ from rich import print
 content = parse_data_as_lines(8)
 result = 0
 
+ops = []
+for line in content:
+    operation, arg = line.split(" ")
+    ops.append((operation, int(arg)))
+
+ptr = 0
+acc = 0
+ptr_seen = []
+while True:
+    operation, arg = ops[ptr]
+    if ptr in ptr_seen:
+        break
+    else:
+        ptr_seen.append(ptr)
+
+    if operation == "nop":
+        ptr += 1
+        continue
+    elif operation == "acc":
+        acc += arg
+        ptr +=1
+        continue
+    elif operation == "jmp":
+        ptr += arg
+        continue
+    else:
+        raise ValueError("Shouldn't reach this")
+
+result = acc
 
 print(f"Part 1: {result}, {elapsed(start_time)}")
--- day08/day08-part 2.py
+++ day08/day08-part 2.py
@@ -2,9 +2,55 @@
 # Author: Jarro van Ginkel
 from aoc_utils import elapsed, parse_data_as_lines, start_time
 from rich import print
+from copy import deepcopy
 
 content = parse_data_as_lines(8)
 result = 0
 
+ops = []
+for line in content:
+    operation, arg = line.split(" ")
+    ops.append((operation, int(arg)))
+
+ptr = 0
+acc = 0
+ptr_seen = []
+
+for idx, (operation, arg) in enumerate(ops):
+    if operation not in ["jmp", "nop"]:
+        continue
+    new_ops = deepcopy(ops)
+    new_ops[idx] = ("jmp", arg) if operation == "nop" else ("nop", arg)
+
+    looped = False
+
+    ptr = 0
+    acc = 0
+    ptr_seen = []
+    while True:
+        if ptr in ptr_seen:
+            looped = True
+            break
+        elif ptr == len(new_ops):
+            break
+
+        op, ar = new_ops[ptr]
+        ptr_seen.append(ptr)
+        if op == "nop":
+            ptr += 1
+            continue
+        elif op == "acc":
+            acc += ar
+            ptr +=1
+            continue
+        elif op == "jmp":
+            ptr += ar
+            continue
+        else:
+            raise ValueError("Shouldn't reach this")
+
+    if not looped:
+        result = acc
+        break
 
 print(f"Part 2: {result}, {elapsed(start_time)}")
