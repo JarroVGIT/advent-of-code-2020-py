30821479de48d443fc0dd973848b2e2b7fed3d87
JarroVGIT <jarro@vanginkel-it.nl>
2024-12-19 15:47:56 +0100

    Day 12

--- day12/day12-part 1.py
+++ day12/day12-part 1.py
@@ -6,5 +6,42 @@ from rich import print
 content = parse_data_as_lines(12)
 result = 0
 
+ops = []
+for line in content:
+    ops.append((line[0], int(line[1:])))
+
+current_dir = "E"
+current_loc = complex(0,0)
+rotate_r = ["N", "E", "S", "W", "N", "E", "S", "W"]
+rotate_l = ["N", "W", "S", "E", "N", "W", "S", "E"]
+
+def do_op(current_loc: complex, current_dir: str, op: tuple[str, int]):
+    operation, amount = op
+    match operation:
+        case "N":
+            return current_loc + (amount * complex(0,-1)), current_dir
+        case "E":
+            return current_loc + (amount * complex(1,0)), current_dir
+        case "S":
+            return current_loc + (amount * complex(0,1)), current_dir
+        case "W":
+            return current_loc + (amount * complex(-1,0)), current_dir
+        case "F":
+            return do_op(current_loc, current_dir, (current_dir, amount))
+        case "R":
+            idx = rotate_r.index(current_dir)
+            next_idx = amount // 90
+            return current_loc, rotate_r[idx+next_idx]
+        case "L":
+            idx = rotate_l.index(current_dir)
+            next_idx = amount // 90
+            return current_loc, rotate_l[idx+next_idx]
+        case _:
+            raise ValueError()
+
+for op in ops:
+    current_loc, current_dir = do_op(current_loc, current_dir, op)
+
+result = abs(int(current_loc.real)) + abs(int(current_loc.imag))
 
 print(f"Part 1: {result}, {elapsed(start_time)}")
--- day12/day12-part 2.py
+++ day12/day12-part 2.py
@@ -6,5 +6,39 @@ from rich import print
 content = parse_data_as_lines(12)
 result = 0
 
+ops = []
+for line in content:
+    ops.append((line[0], int(line[1:])))
+
+current_dir = "E"
+current_loc = complex(0,0)
+rotate_r = {90: 1j, 180: -1, 270: -1j}
+rotate_l = {90: -1j, 180: -1, 270: 1j}
+
+def do_op(current_loc: complex, waypoint: complex, op: tuple[str, int]):
+    operation, amount = op
+    match operation:
+        case "N":
+            return current_loc, waypoint + (amount * complex(0,-1))
+        case "E":
+            return current_loc, waypoint + (amount * complex(1,0))
+        case "S":
+            return current_loc, waypoint + (amount * complex(0,1))
+        case "W":
+            return current_loc, waypoint + (amount * complex(-1,0))
+        case "F":
+            return current_loc + waypoint*amount, waypoint
+        case "R":
+            return current_loc, waypoint * rotate_r[amount]
+        case "L":
+            return current_loc, waypoint * rotate_l[amount]
+        case _:
+            raise ValueError()
+
+waypoint = complex(10,-1)
+for op in ops:
+    current_loc, waypoint = do_op(current_loc, waypoint, op)
+
+result = abs(int(current_loc.real)) + abs(int(current_loc.imag))
 
 print(f"Part 2: {result}, {elapsed(start_time)}")
