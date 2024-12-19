b085ac21246dbefa0ff4d3d8dc7ae25952d76d42
JarroVGIT <jarro@vanginkel-it.nl>
2024-12-19 12:28:22 +0100

    Day 6

--- day06/day06-part 1.py
+++ day06/day06-part 1.py
@@ -3,8 +3,16 @@
 from aoc_utils import elapsed, parse_data_as_lines, start_time
 from rich import print
 
-content = parse_data_as_lines(6)
+content = parse_data_as_lines(6, "\n\n")
 result = 0
+
+groups = [block.split("\n") for block in content]
+
+for group in groups:
+    yesses = set()
+    for person in group:
+        yesses.update(list(person))
+    result += len(yesses)
 
 
 print(f"Part 1: {result}, {elapsed(start_time)}")
--- day06/day06-part 2.py
+++ day06/day06-part 2.py
@@ -3,8 +3,12 @@
 from aoc_utils import elapsed, parse_data_as_lines, start_time
 from rich import print
 
-content = parse_data_as_lines(6)
+content = parse_data_as_lines(6, "\n\n")
 result = 0
+groups = [block.split("\n") for block in content]
 
+for group in groups:
+    yesses = set.intersection(*[set(list(person)) for person in group])
+    result += len(yesses)
 
 print(f"Part 2: {result}, {elapsed(start_time)}")
