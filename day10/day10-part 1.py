43e46977e6edfce01663615d81297cb7de2261ff
JarroVGIT <jarro@vanginkel-it.nl>
2024-12-19 14:45:16 +0100

    Day 10

--- day10/day10-part 1.py
+++ day10/day10-part 1.py
@@ -6,5 +6,14 @@ from rich import print
 content = parse_data_as_lines(10)
 result = 0
 
+adapters = [int(line) for line in content]
+adapters.append(0)
+adapters.append(max(adapters)+3)
+adapters.sort()
+
+one_jolts = sum(b-a == 1 for a,b in zip(adapters, adapters[1:]))
+three_jolts = sum(b-a == 3 for a, b in zip(adapters, adapters[1:]))
+
+result = one_jolts * three_jolts
 
 print(f"Part 1: {result}, {elapsed(start_time)}")
--- day10/day10-part 2.py
+++ day10/day10-part 2.py
@@ -2,9 +2,22 @@
 # Author: Jarro van Ginkel
 from aoc_utils import elapsed, parse_data_as_lines, start_time
 from rich import print
+from collections import defaultdict
 
 content = parse_data_as_lines(10)
 result = 0
 
+adapters = [int(line) for line in content]
+adapters.append(0)
+adapters.append(max(adapters)+3)
+adapters.sort()
+
+counts = defaultdict(int)
+counts[0] = 1
+
+for adapter in adapters[1:]:
+    counts[adapter] = counts[adapter-1] + counts[adapter-2] + counts[adapter-3]
+
+result = counts[adapters[-1]]
 
 print(f"Part 2: {result}, {elapsed(start_time)}")
