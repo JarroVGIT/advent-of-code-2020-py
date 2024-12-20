8ab48a284295f67b0f8666359ecbbfe9d0f92b0a
JarroVGIT <jarro@vanginkel-it.nl>
2024-12-20 15:29:41 +0100

    Day 18

--- day18/day18-part 1.py
+++ day18/day18-part 1.py
@@ -3,8 +3,26 @@
 from aoc_utils import elapsed, parse_data_as_lines, start_time
 from rich import print
 
-content = parse_data_as_lines(18)
+problems = parse_data_as_lines(18)
 result = 0
 
+def calc(input: str) -> int:
+    res = 0
+    op = "+"
+    for c in input.split(" "):
+        if c.isdigit():
+            res = eval(f"{res}{op}{c}")
+        else:
+            op = c
+    return res
+
+# remove all parentheses first
+for problem in problems:
+    while (close:=problem.find(")")) > 0:
+        open = problem[:close].rfind("(")
+        e = calc(problem[open+1:close])
+        problem = problem[:open] + str(e) + problem[close+1:]
+
+    result += calc(problem)
 
 print(f"Part 1: {result}, {elapsed(start_time)}")
--- day18/day18-part 2.py
+++ day18/day18-part 2.py
@@ -3,8 +3,34 @@
 from aoc_utils import elapsed, parse_data_as_lines, start_time
 from rich import print
 
-content = parse_data_as_lines(18)
+problems = parse_data_as_lines(18)
 result = 0
 
+def calc(input: str) -> int:
+    res = 0
+    op = "+"
+    parts = input.split(" ")
+    while True:
+        try:
+            add = parts.index("+")
+        except ValueError:
+            break
+        parts = parts[:add-1] + [str(int(parts[add-1]) + int(parts[add+1]))] + parts[add+2:]
+    for c in parts:
+        if c.isdigit():
+            res = eval(f"{res}{op}{c}")
+        else:
+            op = c
+    return res
+
+# remove all parentheses first
+for problem in problems:
+    while (close:=problem.find(")")) > 0:
+        open = problem[:close].rfind("(")
+        e = calc(problem[open+1:close])
+        problem = problem[:open] + str(e) + problem[close+1:]
+
+    result += calc(problem)
+    
 
 print(f"Part 2: {result}, {elapsed(start_time)}")
