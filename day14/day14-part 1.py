3225efbb86fa39ef3b1ec3c7b78a0e5629a7951e
JarroVGIT <jarro@vanginkel-it.nl>
2024-12-20 11:45:05 +0100

    Day 14

--- day14/day14-part 1.py
+++ day14/day14-part 1.py
@@ -2,9 +2,43 @@
 # Author: Jarro van Ginkel
 from aoc_utils import elapsed, parse_data_as_lines, start_time
 from rich import print
+import sys
 
 content = parse_data_as_lines(14)
 result = 0
 
+memory = {}
 
+def get_mask_ints(mask: str) -> tuple[int, int]:
+    """
+    The mask to be applied is both an OR and AND, so we construct 
+    integers for both the OR part and the AND part.
+    """
+    reversed = list(mask)
+    reversed.reverse()
+    
+    max = sys.maxsize * 2 + 1
+    
+    or_decimal = 0
+    and_decimal = 0
+
+    for idx, bit in enumerate(reversed):
+        if bit == "1":
+            or_decimal += 2**idx
+        elif bit == "0":
+            and_decimal += 2**idx
+
+    and_decimal = max-and_decimal
+    return or_decimal, and_decimal
+
+for line in content:
+    if line.startswith("mask = "):
+        mask: tuple[int,int] = get_mask_ints(line.replace("mask = ", ""))
+    else:
+        mem, val = line.split(" = ")
+        mem = int(mem.replace("mem[", "").replace("]",""))
+        val = int(val)
+        memory[mem] = (mask[0] | val) & mask[1]
+
+result = sum(memory.values())
 print(f"Part 1: {result}, {elapsed(start_time)}")
--- day14/day14-part 2.py
+++ day14/day14-part 2.py
@@ -2,9 +2,42 @@
 # Author: Jarro van Ginkel
 from aoc_utils import elapsed, parse_data_as_lines, start_time
 from rich import print
-
+from itertools import product
 content = parse_data_as_lines(14)
 result = 0
 
+memory = {}
+
+def get_address_spaces(mask: str, mem_loc: int) -> list[int]:
+    """
+    Given a mem_loc (int) and a mask (containing floating bits), construct
+    all possible memory locations. 
+    """
+    mem_loc = list(bin(mem_loc).replace("0b", ""))
+    mem_loc = (["0"] * (len(mask)-len(mem_loc))) + mem_loc
+    
+    one_positions = [pos for pos, char in enumerate(mask) if char == "1"]
+    for pos in one_positions:
+        mem_loc[pos] = "1"
+    
+    x_positions = [pos for pos, char in enumerate(mask) if char == 'X']
+    mem_locs = []
+    for combination in product('01', repeat=len(x_positions)):
+        for pos, bit in zip(x_positions, combination):
+            mem_loc[pos] = bit
+        mem_locs.append(int("".join(mem_loc), base=2))
+    return mem_locs
+
+for line in content:
+    if line.startswith("mask = "):
+        mask = line.replace("mask = ", "")
+    else:
+        mem, val = line.split(" = ")
+        mem = int(mem.replace("mem[", "").replace("]",""))
+        val = int(val)
+        for space in get_address_spaces(mask, mem):
+            memory[space] = val  
+
+result = sum(memory.values())
 
 print(f"Part 2: {result}, {elapsed(start_time)}")
